1ee2b1e7423ca477cedb90e14beb29e7c3637a3d
José Mariano Reyes <josemarianoreyesh02@gmail.com>
2024-04-12 10:30:53 -0600

    nuevo live

--- assets/js/popup.js
+++ assets/js/popup.js
@@ -1,7 +1,7 @@
 window.addEventListener("load",function(){
     setTimeout(
         function open(event){
-            document.querySelector(".popup2").style.display="none";
+            document.querySelector(".popup2").style.display="block";
         },1000
     )
 });
